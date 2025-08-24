b3125ed6bb7e4526390b94cb7231de1e0c00b12d
ThaysonScript <thaysonguedes01@gmail.com>
2025-08-24 19:40:29 -0300

    melhorando os atributos de produto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-
-
 import apresentacao.Cli;
 
 public class Main {
--- src/negocio/entidade/Produtos.java
+++ src/negocio/entidade/Produtos.java
@@ -1,46 +1,91 @@
 package negocio.entidade;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-public class Produtos {
-    private String identificadorUnico;
+public abstract class Produtos {
+    private UUID id;
     private String nome;
     private String descricao;
-    private float preco;
+    private float precoCusto;
+    private float precoVenda;
+    private int estoqueAtual;
+    private boolean ativo;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataUltimaAtualizacao;
 
-
-    private void setIdentificadorUnico(String identificador) {
-        this.identificadorUnico = identificador;
+    public LocalDateTime getDataUltimaAtualizacao() {
+        return dataUltimaAtualizacao;
     }
 
-    private void setNome(String nome) {
-        this.nome = nome;
+    public void setDataUltimaAtualizacao(LocalDateTime dataUltimaAtualizacao) {
+        this.dataUltimaAtualizacao = dataUltimaAtualizacao;
     }
 
-    private void setDescricao(String descricao) {
-        this.descricao = descricao;
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
     }
 
-    private void setPreco(float preco) {
-        this.preco = preco;
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
     }
 
-    public String getNome() {
-        return this.nome;
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public int getEstoqueAtual() {
+        return estoqueAtual;
+    }
+
+    public void setEstoqueAtual(int estoqueAtual) {
+        this.estoqueAtual = estoqueAtual;
+    }
+
+    public float getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(float precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public float getPrecoCusto() {
+        return precoCusto;
+    }
+
+    public void setPrecoCusto(float precoCusto) {
+        this.precoCusto = precoCusto;
     }
 
     public String getDescricao() {
-        return this.descricao;
+        return descricao;
     }
 
-    public float getPreco() {
-        return this.preco;
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 
-    public Produtos(String nome, String descricao, float preco) {
-        setIdentificadorUnico(UUID.randomUUID().toString());
-        setNome(nome);
-        setDescricao(descricao);
-        setPreco(preco);
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Produtos(String nome, String descricao, float precoCusto, float precoVenda, int estoqueAtual, boolean ativo, LocalDateTime dataCriacao, LocalDateTime dataUltimaAtualizacao) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.precoCusto = precoCusto;
+        this.precoVenda = precoVenda;
+        this.estoqueAtual = estoqueAtual;
+        this.ativo = ativo;
+        this.dataCriacao = dataCriacao;
+        this.dataUltimaAtualizacao = dataUltimaAtualizacao;
     }
 }
